38adc5eff52821b1f0154f92803d7a005d92662c
David Stanley <davearthur.stanley@gmail.com>
2017-01-17 17:03:40 -0500

    Updated group_normalize_specgram_to_baseline_time.m to normalize by a range of time values instead of a single time point.

--- analyze_data2/plots/funcs_plots_preferred/group_setup/group_normalize_specgram_to_baseline_time.m
+++ analyze_data2/plots/funcs_plots_preferred/group_setup/group_normalize_specgram_to_baseline_time.m
@@ -6,14 +6,21 @@ function group = group_normalize_specgram_to_baseline_time(group,specgram_baseli
     %normalize_within_elects = 1;   % 0 - normalize by mean of the pre-cue period.
                                     % 1 - normalize by each electrode individually. This within-electrode
                                     %    normalizing removes electrode-electrode variability.
+                                    
+    normalize_across_timerange = 1;
+        normalizing_timerange = 0.25;
 
     for i = 1:length(group)
-        ind = find(group(i).xdata2 >= specgram_baseline_time, 1, 'first');     % Find index to normalize along
+        if ~normalize_across_timerange
+            ind = find(group(i).xdata2 >= specgram_baseline_time, 1, 'first');     % Find index to normalize along
+        else
+            ind = find( (group(i).xdata2 >= specgram_baseline_time - normalizing_timerange) & (group(i).xdata2 <= specgram_baseline_time + normalizing_timerange)); 
+        end
         data = group(i).data;
         if ~normalize_within_elects
-            data = data ./ repmat(mean(data(:,:,ind),2),[1,size(data,2),size(data,3)]);
+            data = data ./ repmat(mean(mean(data(:,:,ind),3),2),[1,size(data,2),size(data,3)]);
         else
-            data = data ./ repmat(data(:,:,ind),[1,1,size(data,3)]);
+            data = data ./ repmat(mean(data(:,:,ind),3),[1,1,size(data,3)]);
         end
 
         data = log(data);
